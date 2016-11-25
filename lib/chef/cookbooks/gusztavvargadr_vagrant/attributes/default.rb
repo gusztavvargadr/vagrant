3671fd5358e85af9d55a2283a57d41e215ae43a6
Gusztáv Varga <me@gusztavvargadr.me>
2016-11-25 16:21:52 +0100

    Add support for enabling Windows native packages #10

--- lib/chef/cookbooks/gusztavvargadr_vagrant/attributes/default.rb
+++ lib/chef/cookbooks/gusztavvargadr_vagrant/attributes/default.rb
@@ -1,3 +1,4 @@
 default['gusztavvargadr_vagrant']['windows_features'] = {}
+default['gusztavvargadr_vagrant']['windows_packages'] = {}
 default['gusztavvargadr_vagrant']['chocolatey_packages'] = {}
 default['gusztavvargadr_vagrant']['git_profiles'] = {}
