96da8100c1cc3672735866f6ce8bbe49e3ac1f32
Gusztáv Varga <me@gusztavvargadr.me>
2016-12-03 16:12:02 +0100

    Add all default metadata fields #26

--- lib/chef/cookbooks/gusztavvargadr_vagrant_octopus/metadata.rb
+++ lib/chef/cookbooks/gusztavvargadr_vagrant_octopus/metadata.rb
@@ -1,1 +1,7 @@
 name 'gusztavvargadr_vagrant_octopus'
+maintainer 'Gusztav Varga'
+maintainer_email 'me@gusztavvargadr.me'
+license 'MIT'
+description 'Installs/Configures Octopus'
+long_description 'Installs/Configures Octopus'
+version '0.1.0'
