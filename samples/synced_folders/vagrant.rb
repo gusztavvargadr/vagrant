bcfe495d0e095b4b9bfa355e2672167b7eae4980
Gusztáv Varga <github@gusztavvargadr.me>
2018-09-05 23:15:16 +0200

    Update all synced folder configurations #48

--- samples/synced_folders/vagrant.rb
+++ samples/synced_folders/vagrant.rb
@@ -2,14 +2,21 @@ directory = File.dirname(__FILE__)
 
 require "#{directory}/../vagrant"
 
+VagrantDeployment.defaults_include(
+  'machines' => {
+    'linux' => {
+      'box' => VagrantLinuxServerMachine.defaults['box'],
+    },
+    'windows' => {
+      'box' => VagrantWindowsServerMachine.defaults['box'],
+    },
+  }
+)
+
 VagrantMachine.defaults_include(
-  'box' => VagrantLinuxServerMachine.defaults['box'],
   'synced_folders' => {
     '/vagrant' => {
       'disabled' => true,
-    },
-    '/vagrant-local' => {
-      'source' => '.',
     },
   }
 )
