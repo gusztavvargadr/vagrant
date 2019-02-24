93319f313c73ab6176edc812a965efe6cdcc994c
Gusztáv Varga <human@gusztavvargadr.me>
2019-02-24 13:29:18 +0100

    Update for new box names #53

--- samples/vagrant.rb
+++ samples/vagrant.rb
@@ -2,27 +2,39 @@ directory = File.dirname(__FILE__)
 
 require "#{directory}/../src/vagrant"
 
-class VagrantLinuxServerMachine < VagrantMachine
+class VagrantWindowsDesktopMachine < VagrantMachine
   @defaults = {
-    'box' => ENV['VAGRANT_BOX_LINUX_SERVER'] || 'gusztavvargadr/u16s-dc',
-  }
-end
-
-class VagrantLinuxDesktopMachine < VagrantMachine
-  @defaults = {
-    'box' => ENV['VAGRANT_BOX_LINUX_DESKTOP'] || 'gusztavvargadr/u16d-dc',
+    'box' => ENV['VAGRANT_BOX_WINDOWS_DESKTOP'] || 'gusztavvargadr/windows-10',
   }
 end
 
 class VagrantWindowsServerMachine < VagrantMachine
   @defaults = {
-    'box' => ENV['VAGRANT_BOX_WINDOWS_SERVER'] || 'gusztavvargadr/w16sc-de',
+    'box' => ENV['VAGRANT_BOX_WINDOWS_SERVER'] || 'gusztavvargadr/windows-server',
   }
 end
 
-class VagrantWindowsDesktopMachine < VagrantMachine
+class VagrantLinuxDesktopMachine < VagrantMachine
   @defaults = {
-    'box' => ENV['VAGRANT_BOX_WINDOWS_DESKTOP'] || 'gusztavvargadr/w16s-dc',
+    'box' => ENV['VAGRANT_BOX_LINUX_DESKTOP'] || 'gusztavvargadr/ubuntu-desktop',
+  }
+end
+
+class VagrantLinuxServerMachine < VagrantMachine
+  @defaults = {
+    'box' => ENV['VAGRANT_BOX_LINUX_SERVER'] || 'gusztavvargadr/ubuntu-server',
+  }
+end
+
+class VagrantDockerWindowsMachine < VagrantMachine
+  @defaults = {
+    'box' => ENV['VAGRANT_BOX_DOCKER_WINDOWS'] || 'gusztavvargadr/docker-windows',
+  }
+end
+
+class VagrantDockerLinuxMachine < VagrantMachine
+  @defaults = {
+    'box' => ENV['VAGRANT_BOX_DOCKER_LINUX'] || 'gusztavvargadr/docker-linux',
   }
 end
 
@@ -30,12 +42,12 @@ VagrantDeployment.defaults_include(
   'stack' => 'vagrant',
 
   'machines' => {
+    'windows' => {
+      'box' => VagrantWindowsServerMachine.defaults['box'],
+    },
     'linux' => {
       'box' => VagrantLinuxServerMachine.defaults['box'],
       'azure_image_urn' => 'Canonical:UbuntuServer:16.04-LTS:latest',
-    },
-    'windows' => {
-      'box' => VagrantWindowsServerMachine.defaults['box'],
     },
   }
 )
