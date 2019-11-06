4f778194df9c22f345dcd212528af3e6e1fc219e
Gusztáv Varga <mail@gusztavvargadr.me>
2019-11-06 22:49:31 +0100

    Consolidate samples #68

--- samples/vagrant.rb
+++ samples/vagrant.rb
@@ -2,22 +2,6 @@ directory = File.dirname(__FILE__)
 
 require "#{directory}/../src/vagrant"
 
-VagrantMachine.defaults_include(
-  'providers' => {
-    'virtualbox' => {
-      'memory' => 1024,
-      'cpus' => 1,
-    },
-    'hyperv' => {
-      'memory' => 1024,
-      'cpus' => 1,
-    },
-    'azure' => {
-      'size' => 'Standard_B1s',
-    },
-  }
-)
-
 class VagrantWindowsServerMachine < VagrantMachine
   @defaults = VagrantMachine.defaults.deep_merge(
     'box' => ENV['VAGRANT_BOX_WINDOWS_SERVER'] || 'gusztavvargadr/windows-server',
@@ -47,8 +31,8 @@ class VagrantDockerWindowsMachine < VagrantMachine
     'box' => ENV['VAGRANT_BOX_DOCKER_WINDOWS'] || 'gusztavvargadr/docker-windows',
     'providers' => {
       'azure' => {
-        'image_urn' => ENV['VAGRANT_BOX_WINDOWS_SERVER_AZURE_IMAGE_URN'] || 'MicrosoftWindowsServer:WindowsServer:2019-Datacenter:latest',
-        'managed_image_id' => ENV['VAGRANT_BOX_WINDOWS_SERVER_AZURE_MANAGED_IMAGE_ID'] || '',
+        'image_urn' => ENV['VAGRANT_BOX_DOCKER_WINDOWS_AZURE_IMAGE_URN'] || 'MicrosoftWindowsServer:WindowsServer:2019-Datacenter:latest',
+        'managed_image_id' => ENV['VAGRANT_BOX_DOCKER_WINDOWS_AZURE_MANAGED_IMAGE_ID'] || '',
       },
     }
   )
@@ -59,19 +43,9 @@ class VagrantDockerLinuxMachine < VagrantMachine
     'box' => ENV['VAGRANT_BOX_DOCKER_LINUX'] || 'gusztavvargadr/docker-linux',
     'providers' => {
       'azure' => {
-        'image_urn' => ENV['VAGRANT_BOX_LINUX_SERVER_AZURE_IMAGE_URN'] || 'Canonical:UbuntuServer:16.04-LTS:latest',
-        'managed_image_id' => ENV['VAGRANT_BOX_LINUX_SERVER_AZURE_MANAGED_IMAGE_ID'] || '',
+        'image_urn' => ENV['VAGRANT_BOX_DOCKER_LINUX_AZURE_IMAGE_URN'] || 'Canonical:UbuntuServer:16.04-LTS:latest',
+        'managed_image_id' => ENV['VAGRANT_BOX_DOCKER_LINUX_AZURE_MANAGED_IMAGE_ID'] || '',
       },
     }
   )
 end
-
-VagrantDeployment.defaults_include(
-  'stack' => 'vagrant',
-  'service' => 'samples',
-
-  'machines' => {
-    'windows' => VagrantDockerWindowsMachine.defaults,
-    'linux' => VagrantDockerLinuxMachine.defaults,
-  }
-)
