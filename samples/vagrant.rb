aead5ca170b6f1b1115c58184ae6280de71be28e
Gusztáv Varga <mail@gusztavvargadr.me>
2020-04-10 03:01:21 +0200

    Add core provisioners samples #72

--- samples/vagrant.rb
+++ samples/vagrant.rb
@@ -1,11 +1,9 @@
-directory = File.dirname(__FILE__)
-
-require "#{directory}/../src/vagrant"
-
 class VagrantWindowsServerMachine < VagrantMachine
   @defaults = VagrantMachine.defaults.deep_merge(
     'box' => ENV['VAGRANT_BOX_WINDOWS_SERVER'] || 'gusztavvargadr/windows-server',
     'providers' => {
+      'virtualbox' => {},
+      'hyperv' => {},
       'azure' => {
         'image_urn' => ENV['VAGRANT_BOX_WINDOWS_SERVER_AZURE_IMAGE_URN'] || 'MicrosoftWindowsServer:WindowsServer:2019-Datacenter:latest',
         'managed_image_id' => ENV['VAGRANT_BOX_WINDOWS_SERVER_AZURE_MANAGED_IMAGE_ID'] || '',
@@ -18,6 +16,8 @@ class VagrantLinuxServerMachine < VagrantMachine
   @defaults = VagrantMachine.defaults.deep_merge(
     'box' => ENV['VAGRANT_BOX_LINUX_SERVER'] || 'gusztavvargadr/ubuntu-server',
     'providers' => {
+      'virtualbox' => {},
+      'hyperv' => {},
       'azure' => {
         'image_urn' => ENV['VAGRANT_BOX_LINUX_SERVER_AZURE_IMAGE_URN'] || 'Canonical:UbuntuServer:16.04-LTS:latest',
         'managed_image_id' => ENV['VAGRANT_BOX_LINUX_SERVER_AZURE_MANAGED_IMAGE_ID'] || '',
@@ -30,6 +30,8 @@ class VagrantDockerWindowsMachine < VagrantMachine
   @defaults = VagrantMachine.defaults.deep_merge(
     'box' => ENV['VAGRANT_BOX_DOCKER_WINDOWS'] || 'gusztavvargadr/docker-windows',
     'providers' => {
+      'virtualbox' => {},
+      'hyperv' => {},
       'azure' => {
         'image_urn' => ENV['VAGRANT_BOX_DOCKER_WINDOWS_AZURE_IMAGE_URN'] || 'MicrosoftWindowsServer:WindowsServer:2019-Datacenter:latest',
         'managed_image_id' => ENV['VAGRANT_BOX_DOCKER_WINDOWS_AZURE_MANAGED_IMAGE_ID'] || '',
@@ -42,6 +44,8 @@ class VagrantDockerLinuxMachine < VagrantMachine
   @defaults = VagrantMachine.defaults.deep_merge(
     'box' => ENV['VAGRANT_BOX_DOCKER_LINUX'] || 'gusztavvargadr/docker-linux',
     'providers' => {
+      'virtualbox' => {},
+      'hyperv' => {},
       'azure' => {
         'image_urn' => ENV['VAGRANT_BOX_DOCKER_LINUX_AZURE_IMAGE_URN'] || 'Canonical:UbuntuServer:16.04-LTS:latest',
         'managed_image_id' => ENV['VAGRANT_BOX_DOCKER_LINUX_AZURE_MANAGED_IMAGE_ID'] || '',
