8664893b85e0cdb0655913d32b1352194579b636
Gusztáv Varga <mail@gusztavvargadr.me>
2022-07-31 17:20:44 +0200

    Remove Consul and Vault samples

--- samples/consul/single/core/env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-set -o nounset
-
-echo export CONSUL_HTTP_ADDR=127.0.0.1:8500
-if [ -f ./tmp/acl.json ]; then
-  echo export CONSUL_HTTP_TOKEN=`jq -r .SecretID ./tmp/acl.json`
-fi
--- samples/consul/single/service/env.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-set -o nounset
-
-cd `dirname $0`
-
-bash ../core/env.sh
